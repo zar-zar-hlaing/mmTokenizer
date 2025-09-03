4811c9d21ab88dd63f84b59d99a84605864bbc10
Zar Zar Hlaing <zarzarhlaing12@gmail.com>
2025-09-03 22:28:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     description="Myanmar (Burmese) text syllable and word tokenizer",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author="Zar Zar",
+    author="Zar Zar Hlaing",
     url="https://github.com/zar-zar-hlaing/mmTokenizer",
     packages=find_packages(),
     python_requires=">=3.7",
-    install_requires=[],  # empty since you only use Python stdlib
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Natural Language :: Burmese",
